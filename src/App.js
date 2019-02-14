488863a0cee346771b2b6ee36b23e7205410dc74
Jasmin Skopljak <jasmin_skopljak@hotmail.com>
2019-02-15 00:33:39 +0100

    add current weather info in homepage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/home';
 import LoadingSpinner from './images/loading.gif';
 // import DayDetails from './components/daydetails';
 
-const APIKEY = 'OPENWEATHER API KEY';
+const APIKEY = '4d0c614a0cd30eaf90cffa2b1cf59b2b';
 const NEXTFIVEDAYSAPIURL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 const CURRENTWEATHERAPIURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const city = 'Sarajevo';
@@ -28,7 +28,9 @@ class App extends Component {
       ],
       currentWeather: {
         forecast: null,
-        weatherIcon: null  
+        weatherIcon: null,
+        temperature: null,
+        time: null 
       },
       isLoaded: false,
       error: null
@@ -42,14 +44,13 @@ class App extends Component {
     let apiCallTwo = fetch(`${CURRENTWEATHERAPIURL}${city},${countryCode}&units=metric&APPID=${APIKEY}`)
                       .then( (response) => response.json());
 
-    console.log(apiCallTwo);
-
     Promise.all([apiCallOne, apiCallTwo])
     .then((data) => {
+      console.log(data);
       this.setState({
         isLoaded: true,
         nextFiveDays: this.nextFiveDays(data[0].list),
-        currentWeather: { forecast: data[1].weather[0].description, weatherIcon: data[1].weather[0].icon }
+        currentWeather: { forecast: data[1].weather[0].description, weatherIcon: data[1].weather[0].icon, temperature: data[1].main.temp, time: this.getTimeFromUnixTimestamp(data[1].dt) }
       })
     },
       (error) => {
@@ -68,13 +69,11 @@ class App extends Component {
       );
     }
 
-    console.log(this.state);
-
     return (
       <React.Fragment>
         <Router>
           <div className="weather-app">
-            <Route exact path="/" render={ props => <Home days={this.state.nextFiveDays} currentWeather={this.state.currentWeather} /> } />
+            <Route exact path="/" render={ props => <Home days={this.state.nextFiveDays} currentWeather={this.state.currentWeather} time={this.state.time} /> } />
             {/*<Route path="/:day" component={DayDetails} />*/}
           </div>
         </Router>
@@ -87,7 +86,7 @@ class App extends Component {
     list.forEach( (timeslot) => {
       let obj = {};
       let date = new Date(timeslot.dt*1000);
-      let time = date.toLocaleString('en-US', { hour: 'numeric' });
+      let time = this.getTimeFromUnixTimestamp(timeslot.dt);
       let day = date.toLocaleString('en-US', { weekday: 'long' });
       let dateLabel = date.toLocaleString('en-US', {day: 'numeric', month: 'short'});
       let index = days.findIndex(x => x.name === day);
@@ -105,6 +104,11 @@ class App extends Component {
       }
     });
     return days;
+  }
+
+  getTimeFromUnixTimestamp(time) {
+    let date = new Date(time*1000);
+    return date.toLocaleString('en-US', { hour: 'numeric' });
   }
 }
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,20 +7,26 @@ class Home extends Component {
     return (
       <React.Fragment>
         <h1 className="main-title">Sarajevo</h1>
-        <div className="current-weather">
-          <h4>{this.props.currentWeather.forecast}</h4>
-          <img src={`https://openweathermap.org/img/w/${this.props.currentWeather.weatherIcon}.png`} />
-        </div>
         <div className="forecast-wrapper">
-          {this.props.days.map( day => (
-            <Link to={`${day.urlPath}`} key={day.name} className="day-link">
-              <Route 
-                exact 
-                path="/" 
-                render={props => <Day day={day.name} temp={day.temp} description={day.forecast} label={day.dateLabel} icon={day.weatherIcon} />} 
-              />
-            </Link>
-          ))}
+          <div className="current-weather">
+            <h3>Current Weather - {this.props.currentWeather.time}</h3>
+            <div className="current-weather-image-temperature">
+              <img src={`https://openweathermap.org/img/w/${this.props.currentWeather.weatherIcon}.png`} />
+              <span>{this.props.currentWeather.temperature}&#176;</span>
+            </div>
+            <h4>{this.props.currentWeather.forecast}</h4>
+          </div>
+          <div className="next-five-days">
+            {this.props.days.map( day => (
+              <Link to={`${day.urlPath}`} key={day.name} className="day-link">
+                <Route 
+                  exact 
+                  path="/" 
+                  render={props => <Day day={day.name} temp={day.temp} description={day.forecast} label={day.dateLabel} icon={day.weatherIcon} />} 
+                />
+              </Link>
+            ))}
+          </div>
         </div>
       </React.Fragment>
     )
